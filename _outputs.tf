badad0b8c1a2c67dbef337c0740fbca158ebd3f4
Brad <bradleyrosenlof@gmail.com>
2022-07-26 18:00:21 -0500

    alb_url

--- _outputs.tf
+++ _outputs.tf
@@ -26,6 +26,10 @@ output "nlb_zone_id" {
   value = one(module.nlb[*].nlb_zone_id)
 }
 
+output "alb_url" {
+  value = "https://${module.alb.alb_dns_name}:${var.container_port}"
+}
+
 output "ddb_url" {
   value = "mongodb://${var.ddb_username}:${var.ddb_password}@${module.ddb.endpoint}:${var.ddb_port}/default?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
