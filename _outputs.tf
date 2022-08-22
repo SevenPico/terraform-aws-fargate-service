552a38d7c540353c83bf5f11cacbeb060d5fb765
Dave Raring <d@7pi.co>
2022-08-22 11:13:17 -0500

    fixing outputs to use Route53 generated names

--- _outputs.tf
+++ _outputs.tf
@@ -18,12 +18,12 @@ output "alb_dns_name" {
   value = one(module.alb[*].alb_dns_name)
 }
 
-output "alb_dns_alias" {
-  value = module.alb_dns_context.descriptors["FQDN"]
+output "alb_dns_route53_name" {
+  value = join("", aws_route53_record.alb[*].name)
 }
 
-output "nlb_dns_alias" {
-  value = module.nlb_dns_context.descriptors["FQDN"]
+output "nlb_dns_route53_name" {
+  value = join("", aws_route53_record.nlb[*].name)
 }
 
 output "nlb_dns_name" {
@@ -35,19 +35,27 @@ output "nlb_zone_id" {
 }
 
 output "alb_url" {
-  value = "https://${module.alb_dns_context.descriptors["FQDN"]}:${var.container_port}"
+  value = "https://${join("", aws_route53_record.alb[*].name)}:${var.container_port}"
 }
 
 output "ddb_url" {
-  value = "mongodb://${var.ddb_username}:${var.ddb_password}@${module.ddb_dns_context.descriptors["FQDN"]}:${var.ddb_port}/default?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+  value = "mongodb://${var.ddb_username}:${var.ddb_password}@${join("", aws_route53_record.ddb[*].name)}:${var.ddb_port}/default?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
 
 output "ddb_dns_name" {
   value = module.ddb.endpoint
 }
 
+output "ddb_dns_route53_name" {
+  value = join("", aws_route53_record.ddb[*].name)
+}
+
 output "ddb_reader_dns_name" {
   value = module.ddb.reader_endpoint
+}
+
+output "ddb_reader_dns_route53_name" {
+  value = join("", aws_route53_record.ddb_reader[*].name)
 }
 
 output "id" {
